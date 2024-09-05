3a9056e0d183790ff7d466e263cb9845c74ba6ca
Михаил А. Борисенко <BMA@start-npf.yaroslavl.local>
2024-09-05 11:14:12 +0300

    bma6

--- main.go
+++ main.go
@@ -6,16 +6,29 @@ func main() {
 	fmt.Println("Sleces...")
 	s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
 	fmt.Println(s)
+	s = remove(s, 0)
+	fmt.Println(s)
+	s = remove(s, 9)
+	fmt.Println(s)
+	s = remove(s, 3)
+	fmt.Println(s)
 	s = remove(s, 5)
 	fmt.Println(s)
 }
 
 func remove(slice []int, s int) []int {
+	if s < 0 || s > len(slice) {
+		// ничего удалить нельзя
+		return slice
+	}
+	if s == 0 {
+		// первый элемент
+		return slice[1:]
+	}
+	if s == len(slice) {
+		// последний элемент
+		return slice[:len(slice)-1]
+	}
+	// средние элементы
 	return append(slice[:s], slice[s+1:]...)
-}
-
-func remove1(slice []int, s int) []int {
-	result := make([]int, 0)
-	result = append(slice[:s], slice[s+1:]...)
-	return result
 }
